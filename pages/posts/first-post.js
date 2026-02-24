ab839e5409b6b6af0f39856fd27c53ffcdbc0c5e
alex blade <aalexblade@gmail.com>
2026-02-24 22:24:37 +0200

    added layout component

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,12 +1,19 @@
+
+import Head from 'next/head';
 import Link from 'next/link';
+import Layout from '../../components/layout';
  
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
       <h1>First Post</h1>
+      <p>This is my first post.</p>
       <h2>
         <Link href="/">Back to home</Link>
       </h2>
-    </>
+    </Layout>
   );
 }
