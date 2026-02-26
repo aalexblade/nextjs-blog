b6b940621ae3ab9e6a3231097d7d366d2a16826e
alex blade <aalexblade@gmail.com>
2026-02-26 17:49:42 +0200

    update loyout

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -12,9 +12,6 @@ export default function FirstPost() {
       <Script
         src="https://connect.facebook.net/en_US/sdk.js"
         strategy="lazyOnload"
-        onLoad={() =>
-          console.log(`script loaded correctly, window.FB has been populated`)
-        }
       />
       <h1>First Post</h1>
       <h2>
