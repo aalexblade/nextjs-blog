c574efb055f5e8d6719d319379d0e4c38018011a
alex blade <aalexblade@gmail.com>
2026-02-27 13:57:43 +0200

    fix avatar

--- components/layout.js
+++ components/layout.js
@@ -1,12 +1,12 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import styles from './layout.module.css';
-import utilStyles from '../styles/utils.module.css';
-import Link from 'next/link';
- 
-const name = 'Alex Blade';
-export const siteTitle = 'Next.js Sample Website';
- 
+import Head from "next/head";
+import Image from "next/image";
+import styles from "./layout.module.css";
+import utilStyles from "../styles/utils.module.css";
+import Link from "next/link";
+
+const name = "Alex";
+export const siteTitle = "Next.js Sample Website";
+
 export default function Layout({ children, home }) {
   return (
     <div className={styles.container}>
